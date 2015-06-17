f124a4fa8053dc65bf3004c5efc7eefa5c4f98c3
Kenny <kenny@romhat.net>
2015-06-17 00:15:49 -0700

    Some bashy improvements
    - Specify the shell interpreter
    - Drop extra greps to remove grep by adding character sets
    - Only call the browser once because it is slow
    - Make sure services have time to come up before starting browser
    - cd relative to home

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,16 +1,16 @@
+#!/bin/sh
+
 # Start up the launch tower computer controller
-cd psas/launch-tower-comm/launch-tower-comm
-ps ax | grep -v grep | grep ltc.py || nohup ./ltc.py &
-cd -
+cd ~/psas/launch-tower-comm/launch-tower-comm
+ps ax | grep [l]tc.py || nohup ./ltc.py &
 
 # Start up the rocket flight computer controller
-cd psas/commander
-ps ax | grep -v grep | grep app.py || nohup python app.py &
-firefox "http://localhost:5000"
-cd -
+cd ~/psas/commander
+ps ax | grep [a]pp.py || nohup python app.py &
 
 # Start up the telemetry receiver
-cd psas/telemetry
-ps ax | grep -v grep | grep telemetry.py || nohup ./telemetry.py &
-firefox "http://ground.psas.lan:8080"
-cd -
+cd ~/psas/telemetry
+ps ax | grep [t]elemetry.py || nohup ./telemetry.py &
+
+sleep 1
+exec firefox "http://localhost:5000" "http://ground.psas.lan:8080"
